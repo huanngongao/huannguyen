05a3e02381831566e278b80a71d3ac10f56c9344
huanngongao <nguyenduchuan.an@gmail.com>
2017-08-31 14:51:16 +0700

    add new project

--- myharavan/jquery/js.js
+++ myharavan/jquery/js.js
@@ -1,65 +1,81 @@
+// $(document).ready(function() {
+//     if ($("#item").hasClass('active')) {
+//         $(".slider_title").addClass('move_left');
 
+//     }else {
+//         $(".slider_title").removeClass('move_left');
+//     }
+// });
+$('.owl-carousel-1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('.owl-carousel-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+    $('.owl-carousel-3').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-// nav fixed top
-$(document).ready(function(){
-    // $('#wrap_banner_head').affix({offset: {top: 200} });
-    
-  //   if($('#wrap_banner_head').hasClass('affix')) {
-  //   	$("#wrap_banner_head").animate({
-		//     height: 'toggle'
-		// });
-  //   }
-		
-});
-// end
-// carousel-mid
-
-
-
-// end
-// owl-carousel
-$(document).ready(function() {
-	var owl = $('.owl-carousel3');
-	owl.owlCarousel({
-	margin: 20,
-	navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-	nav: true,
-	loop: true,
-	responsive: {
-	0: {
-		items: 2
-	},
-	768: {
-		items: 3
-	},
-	1200: {
-		items: 4
-		}
-	}
-	});
-});
-// end
-// Tooltip
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
-});
-// end
-// Scroll top
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 1000) {
+        if ($(this).scrollTop() > 100) {
+            $('.top_space').hide();
+            // $('.top_space').hide();
+        } else {
+            $('.top_space').show();
+        }
+    });
+}); 
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
             $('.scrollup').fadeIn();
             $('.scrollup').addClass('move_top');
          //     $(".fixed").animate({
-	        //     height: 'toggle'
-	        // });
+            //     height: 'toggle'
+            // });
         } else {
             $('.scrollup').fadeOut();
              $('.scrollup').removeClass('move_top');
@@ -73,26 +89,5 @@ $(document).ready(function () {
         }, 600);
         return false;
     });
- //    $(document).ready(function(){
-	//     $("button").click(function(){
-	//         $("div").animate({
-	//             height: 'toggle'
-	//         });
-	//     });
-	// });
 
 });
-// end
-function mouseOver() {
-    document.getElementById("img_show").style.display = 'none';
-    // ocument.getElementById("img_show").style.transition = '3s';
-    document.getElementById("img_hide").style.display = 'block';
-}
-
-function mouseOut() {
-     document.getElementById("img_show").style.display = 'block';
-    document.getElementById("img_hide").style.display = 'none';
-}
-$('.carousel').carousel({
-    interval: 2000
-})
